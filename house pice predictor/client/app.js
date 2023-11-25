7aa03493075612703b9340c6d3facf87481cd6e4
surendra064 <129845824+surendra064@users.noreply.github.com>
2023-11-25 20:44:23 +0530

    Add files via upload

---
+++ house pice predictor/client/app.js
@@ -0,0 +1,87 @@
+function getBathValue() {
+    var uiBathrooms = document.getElementsByName("uiBathrooms");
+    for(var i in uiBathrooms) {
+      if(uiBathrooms[i].checked) {
+          return parseInt(i)+1;
+      }
+    }
+    return -1; // Invalid Value
+  }
+  
+  function getBHKValue() {
+    var uiBHK = document.getElementsByName("uiBHK");
+    for(var i in uiBHK) {
+      if(uiBHK[i].checked) {
+          return parseInt(i)+1;
+      }
+    }
+    return -1; // Invalid Value
+  }
+  function getbalconyValue() {
+    var uiBalcony = document.getElementsByName("uiBalcony");
+    for(var i in uiBalcony) {
+      if(uiBalcony[i].checked) {
+          return parseInt(i)+1;
+      }
+    }
+    return -1; // Invalid Value
+  }
+  function onClickedEstimatePrice() {
+    console.log("Estimate price button clicked");
+    var sqft = document.getElementById("uiSqft");
+    var bhk = getBHKValue();
+    var balcony=getbalconyValue();
+    var bathrooms = getBathValue();
+    var location = document.getElementById("uiLocations");
+    var area = document.getElementById("uiarea");
+    var estPrice = document.getElementById("uiEstimatedPrice");
+  
+    var url = "http://127.0.0.1:5000/predict_home_price"; //Use this if you are NOT using nginx which is first 7 tutorials
+    // var url = "/api/predict_home_price"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
+  
+    $.post(url, {
+        total_sqft: parseFloat(sqft.value),
+        balcony:balcony,
+        bhk: bhk,
+        bath: bathrooms,
+        location: location.value,
+        area:area.value
+    },function(data, status) {
+        console.log(data.estimated_price);
+        estPrice.innerHTML = "<h2>" + data.estimated_price.toString() + " Lakh</h2>";
+        console.log(status);
+    });
+  }
+  
+  function onPageLoad() {
+    console.log( "document loaded" );
+    var url = "http://127.0.0.1:5000/get_location_names"; // Use this if you are NOT using nginx which is first 7 tutorials
+    var url_area="http://127.0.0.1:5000/get_area_names"
+    // var url = "/api/get_location_names"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
+    $.get(url,function(data, status) {
+        console.log("got response for get_location_names request");
+        if(data) {
+            var locations = data.locations;
+            var uiLocations = document.getElementById("uiLocations");
+            $('#uiLocations').empty();
+            for(var i in locations) {
+                var opt = new Option(locations[i]);
+                $('#uiLocations').append(opt);
+            }
+        }
+    });
+    $.get(url_area,function(data, status) {
+        console.log("got response for get_location_names request");
+        if(data) {
+            var area = data.locations;
+            var uiarea = document.getElementById("uiarea");
+            $('#uiarea').empty();
+            for(var i in area) {
+                var opt = new Option(area[i]);
+                $('#uiarea').append(opt);
+            }
+        }
+    });
+  }
+  
+  window.onload = onPageLoad;
